cd14232a3db0234269c125de547a105f899a6edd
matiaet98 <matias.exequiel.estevez@gmail.com>
2020-04-01 23:35:22 -0300

    nextcloud with onlyoffice

---
+++ nextcloud/config.php
@@ -0,0 +1,34 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\OC\Memcache\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'oc8nshitivet',
+  'passwordsalt' => 'zbM6QRLFy7iOWTqcGvlHehZ4oMvS8A',
+  'secret' => 'DbrLPKR0eS2FfmOEqHW4R8DEpbY297bGJ1Ho2CWWRCivI8pJ',
+  'trusted_domains' => 
+  array (
+    0 => '11.0.0.5',
+    2 => '10.*'
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'sqlite3',
+  'version' => '18.0.3.0',
+  'overwrite.cli.url' => 'http://11.0.0.5',
+  'installed' => true,
+);
+
